3953018da13af37c7366a6c88245e7f293fd02e2
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-09-06 19:48:06 +0200

    Update 2017-09-06T17:47:59.752Z

--- README.md
+++
@@ -1,8 +0,0 @@
-# Event website for GitHub Field Day
-
-
-## Running locally
-- Clone the repository
-- Make sure you have ruby installed and run `gem install jekyll` to install jekyll
-- Run `npm install` to install npm dependencies
-- Run `npm start` to run the site locally
---
+++ js/main.js
@@ -0,0 +1,1 @@
+var map=document.querySelector(".map-container");if(map){var timer=void 0;map.addEventListener("mouseenter",function(){timer=setTimeout(function(){map.classList.add("is-clickable")},500)}),map.addEventListener("mouseleave",function(){clearTimeout(timer),map.classList.remove("is-clickable")})}
